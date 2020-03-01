a5d879dfbd1a12071f600302ef4263628c9e9cd6
Fanan Dala <fanan.dala@yahoo.com>
2020-03-01 22:59:11 +0100

    lexivcal structure

---
+++ lexical-structure.js
@@ -0,0 +1,21 @@
+// IDENTIFIER is a name used for variables or functions
+var _identifier;
+var $identifier;
+var letterIdentifier;
+
+;(2+1) - 9//put a semicolon before brackets to avoid it being continued with previous line
+
+/**
+ * For the following identifiers, if the there is a line break after them, it will be treated as a semi-colon
+ * 
+ *  return
+ *  break
+ *  continue
+ */
+var
+a
+=
+5
+
+console.log(a)
+// ++ and -- are used as prefix to statement that follows
--- variables.js
+++ variables.js
@@ -1,2 +1,44 @@
-var x = 1 //number
-var x = 2.5 // number too
+/**
+ * Primitives
+ */
+var x; //declares variable
+x = 1; //number
+x = 2.5; // number too
+x = "This is a string from double quotes";
+x = 'This is also a string from single quotes';
+x = true; //positive boolean
+x = false; //negative boolean
+x = null; // there is no value
+x = undefined; //value does not exist (I wonder why JS allows us to assign this to a vairable)
+/**
+ * Objects
+ */
+x = {
+	title: 'this is an object',
+	func: function () {
+		return "Objects can have even functions"
+	},
+	other: {
+		title: "they can take other objects too"
+	},
+	arr: [
+		"and arrays too"
+	]
+};
+//access objects
+console.log(x.title);
+console.log(x.func());
+console.log(x["other"].title);
+console.log(x.arr[0]);
+/**
+ * Arays
+ */
+x = [
+	1,
+	2,
+	{
+		title: "arrays can have objects too"
+	}
+];
+console.log(x[2].title);
+console.log(x.length);//length of array
